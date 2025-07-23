dbb8c064e3ea4457724e19632f6fa5c5bbc88309
Talbi Said <eyestech404@protonmail.com>
2025-07-23 13:39:14 +0000

    feat: Ajout des deux managers Posts et Category

---
+++ managers/CategoryManager.php
@@ -0,0 +1,28 @@
+<?php
+
+class CategoryManager extends AbstractManager
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    
+    public function findAll(): array
+	{
+    	$stmt = $this->db->query("SELECT * FROM posts");
+    	$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    	$categorys = [];
+
+    	foreach ($rows as $row) {
+        	$category = new Post(
+            	$row['title_en'],
+            	$row['description_en'],
+            	$row['title_fr'],
+            	$row['description_fr'],
+        	);
+        	$category->setId($row['id']);
+        	$categorys[] = $category;
+    	}
+    	return $categorys;
+	}
+}
---
+++ managers/PostManager.php
@@ -0,0 +1,30 @@
+<?php
+
+class PostManager extends AbstractManager
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    
+    
+    public function findAll(): array
+	{
+    	$stmt = $this->db->query("SELECT * FROM posts");
+    	$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    	$posts = [];
+
+    	foreach ($rows as $row) {
+        	$post = new Post(
+            	$row['title_en'],
+            	$row['excerpt_en'],
+            	$row['title_fr'],
+            	$row['excerpt_fr'],
+            	$row['category'],
+        	);
+        	$post->setId($row['id']);
+        	$posts[] = $post;
+    	}
+    	return $posts;
+	}
+}
